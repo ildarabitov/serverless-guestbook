3a22067d55a749184bf29596525b40b5961d2a65
ildarabitov <59411308+ildarabitov@users.noreply.github.com>
2021-12-02 22:27:58 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e92e4ae2.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
